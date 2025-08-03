a938e10da7a95a34a7f359a3c7b9ed06717fb0f9
Bruce.wu <bruce.wu@optnet.site>
2025-08-03 08:56:00 +0800

    license

--- src/test/java/tech/bitkernel/devops/package-info.java
+++ src/test/java/tech/bitkernel/devops/package-info.java
@@ -1,1 +1,17 @@
+/*
+ * Bitkernel GTT Java Rules
+ * Copyright (C) 2009-2025 dumasd
+ * wukai213@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
 package tech.bitkernel.devops;
