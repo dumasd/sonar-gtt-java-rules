6d43b09871b5b715e35a028b7c1a751e960cccc6
Bruce.wu <bruce.wu@optnet.site>
2025-08-04 17:45:28 +0800

    Fix bugs

--- src/main/java/tech/bitkernel/devops/sonar/java/checks/GttBackendNamingV2Rule.java
+++ src/main/java/tech/bitkernel/devops/sonar/java/checks/GttBackendNamingV2Rule.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of Bitkernel GTT Java Rules.
+ * Bitkernel GTT Java Rules
+ * Copyright (C) 2009-2025 dumasd
+ * wukai213@gmail.com
  *
- * Bitkernel GTT Java Rules is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
  *
- * Bitkernel GTT Java Rules is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
 package tech.bitkernel.devops.sonar.java.checks;
 
--- src/test/java/tech/bitkernel/devops/SimpleTest.java
+++ src/test/java/tech/bitkernel/devops/SimpleTest.java
@@ -1,4 +1,20 @@
 /*
+ * Bitkernel GTT Java Rules
+ * Copyright (C) 2009-2025 dumasd
+ * wukai213@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+/*
  * This file is part of Bitkernel GTT Java Rules.
  *
  * Bitkernel GTT Java Rules is free software: you can redistribute it and/or modify
